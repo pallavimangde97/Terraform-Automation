2107e7525c777137744490f9927e011e83950698
pallavimangde97 <pallavimangde97@gmail.com>
2025-04-27 14:17:49 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-sample-batch-26"
+    bucket = "mydev-project-terraform-sample-pallavi"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
